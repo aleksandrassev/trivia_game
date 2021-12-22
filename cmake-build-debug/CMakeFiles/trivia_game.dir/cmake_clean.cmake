e1c9d9018cab8a6fcf8d593981cd24cc8b014158
Aleksandras Sevcik <ragnarokas@gmail.com>
2021-12-22 16:04:55 +0200

    empty clion project

---
+++ cmake-build-debug/CMakeFiles/trivia_game.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trivia_game.dir/main.cpp.obj"
+  "trivia_game.exe"
+  "trivia_game.exe.manifest"
+  "trivia_game.lib"
+  "trivia_game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trivia_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
