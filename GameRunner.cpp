21f31d3ead5df167545dc4687977cbe18be61cf7
Aleksandras Sevcik <ragnarokas@gmail.com>
2021-12-22 20:12:55 +0200

    golden_output created

--- GameRunner.cpp
+++ GameRunner.cpp
@@ -5,27 +5,34 @@ static bool notAWinner;
 
 int main()
 {
+    int ext_count = 0;
+    while (ext_count < 50)
+    {
+        srand(ext_count);
 
-	srand(time(NULL));
-	Game aGame;
+        Game aGame;
 
-	aGame.add("Chet");
-	aGame.add("Pat");
-	aGame.add("Sue");
+        aGame.add("Chet");
+        aGame.add("Pat");
+        aGame.add("Sue");
 
-	do
-	{
+        int counter = 0;
 
-		aGame.roll(rand() % 5 + 1);
+        do
+        {
+            counter++;
 
-		if (rand() % 9 == 7)
-		{
-			notAWinner = aGame.wrongAnswer();
-		}
-		else
-		{
-			notAWinner = aGame.wasCorrectlyAnswered();
-		}
-	} while (notAWinner);
+            aGame.roll(rand() % 5 + 1);
 
+            if (rand() % 9 == 7)
+            {
+                notAWinner = aGame.wrongAnswer();
+            } else
+            {
+                notAWinner = aGame.wasCorrectlyAnswered();
+            }
+        } while (counter < 200);
+
+        ext_count++;
+    }
 }
