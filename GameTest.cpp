3303168139b7e632ab5dd6314a351907336525b0
Aleksandras Sevcik <ragnarokas@gmail.com>
2021-12-27 16:47:42 +0200

    implemented test for 50 question creation

--- GameTest.cpp
+++ GameTest.cpp
@@ -31,8 +31,16 @@ TEST_F(GameTest, game_constructor_member_initalization)
     EXPECT_EQ(rockQuestions.front(), "Rock Question 0");
 }
 
-//konstruktoriu tikrinam set/get ar instaliuoja teisingai memberius
-// tikrinam ar 50 ciklu prasisuko
-//iskelti metoda
-//suvienodinti
-//patikrinti ar vektoriuose tas kas turi buti
+TEST_F(GameTest, create_questions)
+{
+    Game game;
+
+    std::list<std::string> popQuestions = game.get_popQuestions();
+    std::list<std::string> scienceQuestions = game.get_scienceQuestions();
+    std::list<std::string> sportsQuestions = game.get_sportsQuestions();
+    std::list<std::string> rockQuestions = game.get_rockQuestions();
+    EXPECT_EQ(popQuestions.size(), 50);
+    EXPECT_EQ(scienceQuestions.size(), 50);
+    EXPECT_EQ(sportsQuestions.size(), 50);
+    EXPECT_EQ(rockQuestions.size(), 50);
+}
